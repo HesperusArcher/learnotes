02c5121e0c20e6371d9758cfdeadcb1afb18aa3f
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-10-10 10:18:22 +0800

    + factorial + function_lists

---
+++ elisp/factorial.el
@@ -0,0 +1,7 @@
+(defun factorial (n)
+  (let ((res 1))
+    (while (> n 1)
+      (setq res (* res n)
+            n (- n 1)))
+    res))
+(factorial 10)
---
+++ elisp/function_lists.el
@@ -0,0 +1,30 @@
+(defun NAME ARGLIST
+  [DOCSTRING]
+  BODY...)
+(defvar SYMBOL &optional INITVALUE
+  DOCSTRING)
+(setq SYM VAL
+      SYM VAL ...)
+(let VARLIST
+  BODY...)
+(let* VARLIST
+  BODY...)
+(lambda ARGS
+  [DOCSTRING]
+  [INTERACTIVE]
+  BODY)
+(progn
+  BODY ...)
+(if COND
+    THEN
+  ELSE...)
+(cond CLAUSES...)
+(when COND
+  BODY ...)
+(unless COND
+  BODY ...)
+(when COND
+  BODY ...)
+(or CONDITIONS ...)
+(and CONDITIONS ...)
+(not OBJECT)
